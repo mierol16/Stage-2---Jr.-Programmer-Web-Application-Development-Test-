cca92d22cbed65a3e6335d1ff6f9acea22fac645
mierol16 <72370240+mierol16@users.noreply.github.com>
2020-10-05 12:50:51 +0800

    Add files via upload

--- index.php
+++ index.php
@@ -27,7 +27,6 @@
     <script src="dist/js/app-style-switcher.js"></script>
     <script src="dist/js/feather.min.js"></script>
     <script src="assets/libs/perfect-scrollbar/dist/perfect-scrollbar.jquery.min.js"></script>
-    <script src="dist/js/sidebarmenu.js"></script>
     <!--Custom JavaScript -->
     <script src="dist/js/custom.min.js"></script>
     <!--This page plugins -->
